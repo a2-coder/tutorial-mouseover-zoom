dc91865b85d6ba8a3558556551d4a46a40aed884
@ <arjunatlast@gmail.com>
2022-12-26 23:02:30 +0530

    isetup: initial setup of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import profileSrc from "./assets/profile.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-gradient-to-tr from-indigo-200 to-indigo-50 relative">
+      <div className="grid grid-cols-12 gap-6 h-full">
+        <div className="col-span-12 md:col-span-6 px-12 flex items-center relative">
+          {/* SVG PARTICLES */}
+          <div className="absolute top-12 right-12 z-[5]">
+            <svg
+              className="w-16 h-16 md:w-32 md:h-32"
+              viewBox="0 0 158 158"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                opacity="0.65"
+                d="M105.214 49.7957L35.7542 101.519L25.6902 15.5035L105.214 49.7957Z"
+                fill="#6366F1"
+              />
+              <path
+                opacity="0.65"
+                d="M141.781 136.398L96.9053 130.552L124.406 94.6114L141.781 136.398Z"
+                fill="#6366F1"
+              />
+            </svg>
+          </div>
+          <div className="absolute bottom-0 md:bottom-16 left-12 md:left-24">
+            <svg className="w-16 h-16 md:w-32 md:h-32" viewBox="0 0 218 187" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path opacity="0.65" d="M139.241 46.2015L110.502 156.471L29.3759 76.4473L139.241 46.2015Z" fill="#6366F1"/>
+              <path opacity="0.65" d="M192.519 137.172L160.697 104.995L204.474 93.5246L192.519 137.172Z" fill="#6366F1"/>
+              <path opacity="0.65" d="M186.019 36.8746L153.989 33.5467L172.886 7.47197L186.019 36.8746Z" fill="#6366F1"/>
+            </svg>
+          </div>
+          {/* CONTENT */}
+          <div className="flex flex-col items-start space-y-4 relative z-10">
+            <div className="flex flex-col items-start space-y-1">
+              <h3 className="text-2xl text-gray-700 opacity-50">
+                React Tutorial
+              </h3>
+              <h1 className="text-5xl text-indigo-600 font-bold leading-[1.2]">
+                Mouse-Over Zoom Effect
+              </h1>
+            </div>
+            <div className="rounded-full bg-gray-50 py-2 pl-2 pr-4 flex items-center space-x-2 border border-indigo-200">
+              <img
+                src={profileSrc}
+                alt="a2.coder"
+                className="w-8 h-8 rounded-full object-cover object-center"
+              />
+              <div className="text-lg font-semibold text-gray-800">
+                a2.coder
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-12 md:col-span-4 md:col-start-9 border-t-8 md:border-t-0 md:border-l-8 border-indigo-500 relative z-10">
+          <canvas className="w-full h-full bg-gray-100 cursor-crosshair" /> {/* MAIN IMAGE */}
+          <canvas className="absolute pointer-events-none bottom-full translate-y-1/2 left-1/2 -translate-x-1/2 md:translate-y-0 md:translate-x-0 md:bottom-16 md:-left-16 border-8 border-indigo-500 w-32 h-32 z-10 bg-gray-200" />{" "}
+          {/* ZOOMED IMAGE */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
