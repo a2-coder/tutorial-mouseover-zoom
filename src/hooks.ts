f6817285766b373151b2b899fe68037f302b05e0
@ <arjunatlast@gmail.com>
2022-12-27 22:53:09 +0530

    step: created hook to capture mouse position

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+function useMouse(ref: React.RefObject<HTMLElement>) {
+  const [mouse, setMouse] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  useEffect(() => {
+    if (ref.current) {
+      const handleMouseMove = (e: MouseEvent) => {
+        // get mouse position relative to ref
+        const rect = ref.current?.getBoundingClientRect();
+        if (rect) {
+          setMouse({
+            x: e.clientX - rect.left,
+            y: e.clientY - rect.top,
+          });
+        }
+      };
+      ref.current.addEventListener("mousemove", handleMouseMove);
+      return () => {
+        ref.current!.removeEventListener("mousemove", handleMouseMove)
+      };
+    }
+  });
+  return mouse;
+}
 
 export function useMouseOverZoom(
   source: React.RefObject<HTMLImageElement>,
   target: React.RefObject<HTMLCanvasElement>
 ) {
-  // TODO: implement
+  // CAPTURE MOUSE POSITION
+  const { x, y } = useMouse(source);
 }
